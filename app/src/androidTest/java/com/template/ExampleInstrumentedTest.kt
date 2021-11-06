393c5dc88a1af1c9aba61f98eeb32eece0338e3e
amcneilly <amcneilly331@gmail.com>
2021-11-05 20:39:01 -0400

    Fixing UI test.

--- app/src/androidTest/java/com/template/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/template/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.template", appContext.packageName)
+        assertEquals("com.adammcneilly.TODOREPLACE", appContext.packageName)
     }
 }
