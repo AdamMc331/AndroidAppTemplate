dbecf47fa4bfecdc437ed84dbf49f089105971cf
Adam McNeilly <amcneilly331@gmail.com>
2023-03-10 13:35:25 -0500

    Using gradle version catalogs to share compileSdk versions.

--- mvi/build.gradle.kts
+++ mvi/build.gradle.kts
@@ -5,10 +5,11 @@ plugins {
 
 android {
     namespace = "template.mvi"
-    compileSdk = 33
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        minSdk = 21
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.compileSdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
