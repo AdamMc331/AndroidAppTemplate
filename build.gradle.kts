d240e4e08b1c705e8ead71d7ea80a4e1c8017d30
Vahid Garousi <garousiv@gmail.com>
2023-09-22 15:39:36 +0330

    Refactor project structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,64 +1,64 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-
-    repositories {
-        google()
-        mavenCentral()
-        maven(url = "https://plugins.gradle.org/m2/")
-    }
-
-    dependencies {
-        classpath(libs.detekt.gradle.plugin)
-        classpath(libs.gradle)
-        classpath(libs.gradle.versions.plugin)
-        classpath(libs.kotlin.gradle.plugin)
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
+import com.android.build.gradle.internal.tasks.factory.dependsOn
+import java.util.Locale
 
 plugins {
-    id("com.google.dagger.hilt.android").version(libs.versions.hilt).apply(false)
-    id("io.gitlab.arturbosch.detekt").version(libs.versions.detektGradlePlugin)
-    id("org.jmailen.kotlinter").version(libs.versions.kotlinter).apply(false)
+    libs.plugins.apply {
+        alias(android.application) apply false
+        alias(kotlin.parcelize) apply false
+        alias(android.library) apply false
+        alias(kotlin.android) apply false
+        alias(hilt.android) apply false
+        alias(kapt) apply false
+        alias(detekt) apply false
+        alias(kotliner) apply false
+        alias(ksp) apply false
+    }
 }
 
-apply(from = "buildscripts/githooks.gradle")
-apply(from = "buildscripts/setup.gradle")
-apply(from = "buildscripts/versionsplugin.gradle")
-
-subprojects {
-    apply(from = "../buildscripts/detekt.gradle")
-}
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
+}
+
+
+tasks.register("copyGitHooks", Exec::class.java) {
+    description = "Copies the git hooks from /git-hooks to the .git folder."
+    group = "git hooks"
+
+    workingDir = File("$projectDir")
+
+    val command = """
+       .\git-hooks\setup.sh
+    """.trimIndent()
+
+    if (System.getProperty("os.name").lowercase(Locale.ROOT).contains("windows")) {
+        commandLine("cmd", "/c", command)
+    } else {
+        commandLine("sh", "/c", command)
+    }
+}
+
+
+
+tasks.register("installGitHooks", Exec::class.java) {
+    description = "Installs the pre-commit git hooks from /git-hooks."
+    group = "git hooks"
+    workingDir = rootDir
+
+    if (System.getProperty("os.name").lowercase(Locale.ROOT).contains("windows")) {
+        logger.info("Windows")
+        commandLine("cmd", "-R", "+x", ".git/hooks/")
+    } else {
+        logger.info("Windows")
+        commandLine("sh", "-R", "+x", ".git/hooks/")
+    }
+    dependsOn("copyGitHooks")
+    doLast {
+        logger.info("Git hook installed successfully.")
+    }
 }
 
 afterEvaluate {
     // We install the hook at the first occasion
-    tasks.named("clean") {
-        dependsOn(":installGitHooks")
-    }
-}
-
-tasks {
-    /**
-     * The detektAll tasks enables parallel usage for detekt so if this project
-     * expands to multi module support, detekt can continue to run quickly.
-     *
-     * https://proandroiddev.com/how-to-use-detekt-in-a-multi-module-android-project-6781937fbef2
-     */
-    @Suppress("UnusedPrivateMember")
-    val detektAll by registering(io.gitlab.arturbosch.detekt.Detekt::class) {
-        parallel = true
-        setSource(files(projectDir))
-        include("**/*.kt")
-        include("**/*.kts")
-        exclude("**/resources/**")
-        exclude("**/build/**")
-        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
-        buildUponDefaultConfig = false
-    }
+    tasks.named("clean").dependsOn(":installGitHooks")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,17 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 rootProject.name = "template"
