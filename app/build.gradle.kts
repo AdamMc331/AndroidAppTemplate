a6f50908220619c6f6417253ef675c49dd4544a2
Vahid Garousi <garousiv@gmail.com>
2023-09-22 15:39:01 +0330

    Refactor app level build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,14 @@
 plugins {
-    id("com.android.application")
-    id("com.google.dagger.hilt.android")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    id("org.jmailen.kotlinter")
+    id("template.android.application")
+    id("template.android.application.compose")
+    id("template.android.room")
+    id("template.android.hilt")
 }
 
 android {
-    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "template.app.id"
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.compileSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -29,24 +25,7 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
-    }
-
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -56,22 +35,15 @@ android {
 }
 
 dependencies {
-    val composeBom = platform(libs.compose.bom)
-    implementation(composeBom)
-    debugImplementation(composeBom)
-    androidTestImplementation(composeBom)
-
     implementation(libs.accompanist.systemuicontroller)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.room.runtime)
     implementation(libs.android.material)
     implementation(libs.compose.ui)
     implementation(libs.compose.material)
     implementation(libs.compose.ui.tooling)
-    implementation(libs.hilt.android)
     implementation(libs.square.moshi.kotlin)
     implementation(libs.square.retrofit)
     implementation(libs.square.retrofit.converter.moshi)
@@ -86,11 +58,4 @@ dependencies {
     debugImplementation(libs.compose.ui.test.manifest)
     debugImplementation(libs.compose.ui.tooling)
     debugImplementation(libs.square.leakcanary)
-
-    kapt(libs.androidx.room.compiler)
-    kapt(libs.hilt.compiler)
-    kapt(libs.square.moshi.kotlin.codegen)
-    kaptAndroidTest(libs.hilt.android.compiler)
-
-    annotationProcessor(libs.androidx.room.compiler)
 }
