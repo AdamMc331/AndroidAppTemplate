0a50c3d7f7b449ba5ebc66f2bc024ffcbf5ff6cf
Vahid Garousi <garousiv@gmail.com>
2023-09-22 15:41:32 +0330

    Add `detektAll` command

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,3 +62,24 @@ afterEvaluate {
     // We install the hook at the first occasion
     tasks.named("clean").dependsOn(":installGitHooks")
 }
+
+
+tasks {
+    /**
+     * The detektAll tasks enables parallel usage for detekt so if this project
+     * expands to multi module support, detekt can continue to run quickly.
+     *
+     * https://proandroiddev.com/how-to-use-detekt-in-a-multi-module-android-project-6781937fbef2
+     */
+    @Suppress("UnusedPrivateMember")
+    val detektAll by registering(io.gitlab.arturbosch.detekt.Detekt::class) {
+        parallel = true
+        setSource(files(projectDir))
+        include("**/*.kt")
+        include("**/*.kts")
+        exclude("**/resources/**")
+        exclude("**/build/**")
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = false
+    }
+}
