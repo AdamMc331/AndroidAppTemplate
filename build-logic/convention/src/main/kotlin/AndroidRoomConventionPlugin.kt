527be2f84d2d48c14bdddf8f0484e9d7eb7e45e8
Vahid Garousi <garousiv@gmail.com>
2023-09-22 15:57:20 +0330

    Remove useless code from AndroidRoomConventionPlugin.kt

--- build-logic/convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
@@ -31,30 +31,11 @@ class AndroidRoomConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("com.google.devtools.ksp")
-
-            extensions.configure<KspExtension> {
-                // The schemas directory contains a schema file for each version of the Room database.
-                // This is required to enable Room auto migrations.
-                // See https://developer.android.com/reference/kotlin/androidx/room/AutoMigration.
-//                arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
-            }
             dependencies {
                 add("implementation", libs.findLibrary("androidx-room-runtime").get())
 //                add("implementation", libs.findLibrary("room.ktx").get())
                 add("ksp", libs.findLibrary("androidx-room-compiler").get())
             }
         }
-    }
-
-    /**
-     * https://issuetracker.google.com/issues/132245929
-     * [Export schemas](https://developer.android.com/training/data-storage/room/migrating-db-versions#export-schemas)
-     */
-    class RoomSchemaArgProvider(
-        @get:InputDirectory
-        @get:PathSensitive(PathSensitivity.RELATIVE)
-        val schemaDir: File,
-    ) : CommandLineArgumentProvider {
-        override fun asArguments() = listOf("room.schemaLocation=${schemaDir.path}")
     }
 }
