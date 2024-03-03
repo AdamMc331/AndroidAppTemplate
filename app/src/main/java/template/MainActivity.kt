c4ddb0500ea6a9e7cf05f7d494744ef25fa07fc9
Adam McNeilly <amcneilly331@gmail.com>
2024-03-02 19:39:03 -0500

    Removing accompanist systemuicontroller. (#136)
    Fixes #135.

--- app/src/main/java/template/MainActivity.kt
+++ app/src/main/java/template/MainActivity.kt
@@ -3,25 +3,20 @@ package template
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.graphics.Color
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import template.theme.TemplateTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        configureEdgeToEdgeWindow()
-
         setContent {
-            ConfigureTransparentSystemBars()
+            ConfigureEdgeToEdgeWindow()
 
             TemplateTheme {
                 Surface(
@@ -34,32 +29,16 @@ class MainActivity : ComponentActivity() {
     }
 
     /**
-     * Configures our [MainActivity] window so that it reaches edge to edge of the device, meaning
-     * content can be rendered underneath the status and navigation bars.
+     * Enables edge to edge support of this activity.
      *
-     * This method works hand in hand with [ConfigureTransparentSystemBars], to make sure content
-     * behind these bars is visible.
-     *
-     * Keep in mind that if you need to make sure your content padding doesn't clash with the status bar text/icons,
-     * you can leverage modifiers like `windowInsetsPadding()` and `systemBarsPadding()`. For more information,
-     * read the Compose WindowInsets docs: https://developer.android.com/reference/kotlin/androidx/compose/foundation/layout/WindowInsets
+     * If you'd like, you can override the default behavior of system bars by
+     * customizing the parameters in the call to [enableEdgeToEdge].
      */
-    private fun configureEdgeToEdgeWindow() {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-    }
-
     @Composable
-    private fun ConfigureTransparentSystemBars() {
-        val systemUiController = rememberSystemUiController()
-        val useDarkIcons = !isSystemInDarkTheme()
-
-        DisposableEffect(systemUiController, useDarkIcons) {
-            systemUiController.setSystemBarsColor(
-                color = Color.Transparent,
-                darkIcons = useDarkIcons,
-            )
-
-            onDispose { }
+    private fun ConfigureEdgeToEdgeWindow() {
+        DisposableEffect(Unit) {
+            enableEdgeToEdge()
+            onDispose {}
         }
     }
 }
