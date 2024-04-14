2511fdb299042bad4bf16e824713833a63bbf228
Adam McNeilly <amcneilly331@gmail.com>
2024-04-14 13:52:55 -0400

    Tinkering with state across config changes.

--- app/src/main/java/template/MainActivity.kt
+++ app/src/main/java/template/MainActivity.kt
@@ -1,28 +1,78 @@
 package template
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import template.navigation.DynamicNavigationContainer
+import template.navigation.NavigationTab
+import template.navigation.NavigationTabDisplayModel
+import template.navigation.NavigationType
 import template.theme.TemplateTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Log.d("ADAMLOG", "OnCreate")
+
         setContent {
             ConfigureEdgeToEdgeWindow()
 
-            TemplateTheme {
+            AdaptiveNavigationExample()
+        }
+    }
+
+    @Composable
+    private fun AdaptiveNavigationExample() {
+        TemplateTheme {
+            var selectedTab by rememberSaveable {
+                mutableStateOf(NavigationTab.ONE)
+            }
+
+            val tabs = NavigationTab.entries.map { tab ->
+                NavigationTabDisplayModel(
+                    tab = tab,
+                    selected = (selectedTab == tab),
+                    onClick = {
+                        selectedTab = tab
+                    },
+                )
+            }
+
+            DynamicNavigationContainer(
+                navigationItems = tabs,
+                navigationType = NavigationType.fromActivity(activity = this),
+            ) {
                 Surface(
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    var isChecked by rememberSaveable {
+                        mutableStateOf(false)
+                    }
+
+                    Column {
+                        Text(text = "Checked: $isChecked")
+
+                        Checkbox(
+                            checked = isChecked,
+                            onCheckedChange = {
+                                isChecked = !isChecked
+                            },
+                        )
+                    }
                 }
             }
         }
