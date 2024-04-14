57f7b7d01b75ddbd1b23a0b77b11367a56a08913
Adam McNeilly <amcneilly331@gmail.com>
2024-04-14 13:38:48 -0400

    Suppressing long comment. Might not merge this.

--- navigation/src/main/java/template/navigation/DynamicNavigationContainer.kt
+++ navigation/src/main/java/template/navigation/DynamicNavigationContainer.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 
+@Suppress("MaxLineLength") // Doc link too long to truncate
 /**
  * This is the main entry point to dynamic navigation. Based on the [navigationType] supplied, we'll render
  * a specific navigation component to show the [navigationItems]. The space not used by the navigation component
