be4f461cfbe462e304e5f3b954229aa9d1c82739
Adam McNeilly <amcneilly331@gmail.com>
2024-11-07 21:55:59 -0500

    Updating Dangerfile with templated checks.

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,29 +1,30 @@
 @file:Suppress("MagicNumber", "WildcardImport")
-// Editing this file: https://github.com/marketplace/actions/danger-kotlin#autocomplete-and-syntax-highlighting-in-intellij-idea-or-android-studio
+
+// Editing this file: https://github.com/danger/kotlin?tab=readme-ov-file#autocomplete-and-syntax-highlighting-in-intellij-idea-or-android-studio
 import systems.danger.kotlin.*
 
 danger(args) {
 
-    val allSourceFiles = git.modifiedFiles + git.createdFiles
-    val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
-    val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
-
     onGitHub {
-        val isTrivial = pullRequest.title.contains("#trivial")
+        val additions = pullRequest.additions ?: 0
+        val deletions = pullRequest.deletions ?: 0
 
-        // Changelog
-        if (!isTrivial && !changelogChanged && sourceChanges != null) {
-            warn("any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md).")
+        message("Thanks @${pullRequest.user.login}!")
+
+        if (pullRequest.body.isNullOrBlank()) {
+            fail("Please provide a summary in the Pull Request description.")
         }
 
-        // Big PR Check
-        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
-            warn("Big PR, try to keep changes smaller if you can")
+        if (additions > 500) {
+            warn("Please consider breaking up this pull request.")
         }
 
-        // Work in progress check
-        if (pullRequest.title.contains("WIP", false)) {
-            warn("PR is classed as Work in Progress")
+        // TODO: Find out if we can look up labels
+
+        if (deletions > additions) {
+            message("🎉 Code Cleanup!")
         }
+
+        // TODO: Copy over old code for dependency update printing
     }
 }
