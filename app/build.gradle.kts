e547ba71f2f93d1251e78976b0fd48f1dca5689b
Adam McNeilly <amcneilly331@gmail.com>
2025-05-23 18:03:22 -0400

    Adding okhttp logging interceptor.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling)
     implementation(libs.hilt.android)
+    implementation(libs.square.logging.interceptor)
     implementation(libs.square.moshi.kotlin)
     implementation(libs.square.retrofit)
     implementation(libs.square.retrofit.converter.moshi)
